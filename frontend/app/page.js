5a3d9d4e8dd70ff9bd28b2fd21360a5f54b3d0bb
HashMaster <hshah86@hawk.iit.edu>
2025-01-20 14:38:30 -0600

    added menu bar

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -7,137 +7,143 @@ import { TopDownList } from '@/components/top-down-list';
 import { SecuritySelect } from '@/components/security-select';
 import { DateRangeSelect } from '@/components/date-range-select';
 import AccountMultiSelect from '@/components/account-multiselect';
+import Menu from '@/components/menu';
 import {
-    fetchCardData,
-    fetchTopDownBottomUpData,
-    fetchAccountsData,
-    fetchSecuritiesData,
-    fetchGraphData,
+	fetchCardData,
+	fetchTopDownBottomUpData,
+	fetchAccountsData,
+	fetchSecuritiesData,
+	fetchGraphData,
 } from '@/data/fetching';
 
 export default function Home() {
-    const [filters, setFilters] = useState({
-        accounts: [''],
-        security: '',
-        start_date: '',
-        end_date: '',
-        pnl_type: '',
-    });
+	const [filters, setFilters] = useState({
+		accounts: [''],
+		security: '',
+		start_date: '',
+		end_date: '',
+		pnl_type: '',
+	});
 
-    const [cardData, setCardData] = useState(null);
-    const [topDownBottomUpData, setTopDownBottomUpData] = useState(null);
-    const [accountsData, setAccountsData] = useState(null);
-    const [securitiesData, setSecuritiesData] = useState(null);
-    const [graphData, setGraphData] = useState(null);
+	const [cardData, setCardData] = useState(null);
+	const [topDownBottomUpData, setTopDownBottomUpData] = useState(null);
+	const [accountsData, setAccountsData] = useState(null);
+	const [securitiesData, setSecuritiesData] = useState(null);
+	const [graphData, setGraphData] = useState(null);
 
-    const fetchAllData = async (newFilters) => {
-        try {
-            const updatedFilters = { ...filters, ...newFilters };
-            setFilters(updatedFilters);
+	const fetchAllData = async newFilters => {
+		try {
+			const updatedFilters = { ...filters, ...newFilters };
+			setFilters(updatedFilters);
 
-            const card = await fetchCardData(updatedFilters);
-            const tdbu = await fetchTopDownBottomUpData(updatedFilters);
-            const graph = await fetchGraphData(updatedFilters);
+			const card = await fetchCardData(updatedFilters);
+			const tdbu = await fetchTopDownBottomUpData(updatedFilters);
+			const graph = await fetchGraphData(updatedFilters);
 
-            setCardData(card);
-            setTopDownBottomUpData(tdbu);
-            setGraphData(graph);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+			setCardData(card);
+			setTopDownBottomUpData(tdbu);
+			setGraphData(graph);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    const refreshGraph = async (newFilters) => {
-        try {
-            const updatedFilters = { ...filters, ...newFilters };
-            setFilters(updatedFilters);
+	const refreshGraph = async newFilters => {
+		try {
+			const updatedFilters = { ...filters, ...newFilters };
+			setFilters(updatedFilters);
 
-            const graph = await fetchGraphData(updatedFilters);
+			const graph = await fetchGraphData(updatedFilters);
 
-            setGraphData(graph);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+			setGraphData(graph);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    useEffect(() => {
-        async function initialLoad() {
-            const accounts = await fetchAccountsData();
-            const securities = await fetchSecuritiesData();
-            const updatedFilters = {
-                ...filters,
-                ...{
-                    accounts: [],
-                    security: securities.data[0],
-                    start_date: '2024-01-01',
-                    end_date: '2024-12-20',
-                    pnl_type: 'total',
-                },
-            };
-            setFilters(updatedFilters);
-            await fetchAllData(updatedFilters);
+	useEffect(() => {
+		async function initialLoad() {
+			const accounts = await fetchAccountsData();
+			const securities = await fetchSecuritiesData();
+			const updatedFilters = {
+				...filters,
+				...{
+					accounts: [],
+					security: securities.data[0],
+					start_date: '2024-01-01',
+					end_date: '2024-12-20',
+					pnl_type: 'total',
+				},
+			};
+			setFilters(updatedFilters);
+			await fetchAllData(updatedFilters);
 
-            const graph = await fetchGraphData(updatedFilters);
-            setAccountsData(accounts);
-            setSecuritiesData(securities);
-            setGraphData(graph);
-        }
+			const graph = await fetchGraphData(updatedFilters);
+			setAccountsData(accounts);
+			setSecuritiesData(securities);
+			setGraphData(graph);
+		}
 
-        initialLoad();
-    }, []);
+		initialLoad();
+	}, []);
 
-    return (
-        <main className="container mx-auto p-4 space-y-4 font-inter">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-                <div className="rounded-xl border border-gray-200 p-4 space-y-6">
-                    <SecuritySelect
-                        value={filters.security}
-                        onChange={fetchAllData}
-                        tickers={
-                            securitiesData && securitiesData.data
-                                ? securitiesData.data
-                                : []
-                        }
-                    />
-                    <DateRangeSelect
-                        startDate={filters.start_date}
-                        endDate={filters.end_date}
-                        onChange={fetchAllData}
-                    />
-                </div>
-                <div className="rounded-xl border border-gray-200 p-4">
-                    <AccountMultiSelect
-                        options={
-                            accountsData && accountsData.data
-                                ? accountsData.data
-                                : []
-                        }
-                        onChange={fetchAllData}
-                    />
-                </div>
-            </div>
-            <MetricCards data={cardData?.data ? cardData.data : null} />
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
-                <PLChart
-                    pnltype={filters.pnl_type}
-                    data={graphData && graphData.data ? graphData.data : null}
-                    onChange={refreshGraph}
-                />
-                <TopDownList
-                    securities={
-                        topDownBottomUpData && topDownBottomUpData.data
-                            ? topDownBottomUpData.data.top_down
-                            : []
-                    }
-                />
-                <BottomUpList
-                    securities={
-                        topDownBottomUpData && topDownBottomUpData.data
-                            ? topDownBottomUpData.data.bottom_up
-                            : []
-                    }
-                />
-            </div>
-        </main>
-    );
+	return (
+		<>
+			<Menu />
+			<main className="container mx-auto p-4 space-y-4 font-inter">
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+					<div className="rounded-xl border border-gray-200 p-4 space-y-6">
+						<SecuritySelect
+							value={filters.security}
+							onChange={fetchAllData}
+							tickers={
+								securitiesData && securitiesData.data
+									? securitiesData.data
+									: []
+							}
+						/>
+						<DateRangeSelect
+							startDate={filters.start_date}
+							endDate={filters.end_date}
+							onChange={fetchAllData}
+						/>
+					</div>
+					<div className="rounded-xl border border-gray-200 p-4">
+						<AccountMultiSelect
+							options={
+								accountsData && accountsData.data
+									? accountsData.data
+									: []
+							}
+							onChange={fetchAllData}
+						/>
+					</div>
+				</div>
+				<MetricCards data={cardData?.data ? cardData.data : null} />
+				<div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
+					<PLChart
+						pnltype={filters.pnl_type}
+						data={
+							graphData && graphData.data ? graphData.data : null
+						}
+						onChange={refreshGraph}
+					/>
+					<TopDownList
+						securities={
+							topDownBottomUpData && topDownBottomUpData.data
+								? topDownBottomUpData.data.top_down
+								: []
+						}
+					/>
+					<BottomUpList
+						securities={
+							topDownBottomUpData && topDownBottomUpData.data
+								? topDownBottomUpData.data.bottom_up
+								: []
+						}
+					/>
+				</div>
+			</main>
+		</>
+	);
 }
---
+++ frontend/components/menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Menu = () => {
+	return (
+		<header className="border-b-2 font-inter">
+			<menu className="container mx-auto px-4 py-2">
+				<ul className="max-w-fit flex gap-4">
+					<li className="py-2 rounded-lg hover:text-red-600 cursor-pointer">
+						Refresh Data
+					</li>
+				</ul>
+			</menu>
+		</header>
+	);
+};
+
+export default Menu;
