f15affd21f21d94ea18fe4b62047a68ebd3a40f5
HashMaster <hshah86@hawk.iit.edu>
2025-02-11 15:46:29 -0600

    added a script to restart the systemd service

---
+++ restart.sh
@@ -0,0 +1,5 @@
+sudo systemctl stop findash_frontend.service
+sudo systemctl stop findash_backend.service
+
+sudo systemctl start findash_frontend.service
+sudo systemctl start findash_backend.service
